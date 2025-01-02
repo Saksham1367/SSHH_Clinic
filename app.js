3cf23cfc2bd4c1a002a138afd84dc902efa03d6a
Saksham Dhingra <sakshamdhingra1305@gmail.com>
2025-01-02 16:12:43 +0530

    backend changes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ if(process.env.NODE_ENV!="production"){
 }
 const express =require("express");
 const app = express();
-//const mongoose = require("mongoose");
-//const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const WarpAsync =require("./ultis/warpasync");
@@ -13,22 +13,17 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.engine("ejs", ejsMate);
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.listen(3000,()=>{
     console.log("server is listening to port no: 3000");
 });
 
-// home page 
-app.get("/",WarpAsync((req,res,next)=>{
-    res.render("homepage.ejs");
-}))
-
 // setting up mongoose
-/*async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/clinic");
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017//clinic");
 }
 
 main()
@@ -37,7 +32,44 @@ main()
 }).catch((err)=>{
     console.log(err);
 })
-*/
+
+
+// signup page
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+});
+
+app.post("/signup", (req, res) => {
+    console.log(req.body);
+    res.send("Signup data received");
+});
+
+// login page
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    res.send("Login data received");
+});
+
+//consult page
+app.get("/consult", (req, res) => {
+    res.render("consult.ejs");
+}); 
+
+app.post("/consult", (req, res) => {
+    console.log(req.body);
+    res.send("Form data received");
+})
+
+// home page 
+app.get("/",WarpAsync((req,res,next)=>{
+    res.render("homepage.ejs");
+}))
+
+
 
 // error handling for page not found
 app.all("*",(req,res,next)=>{
